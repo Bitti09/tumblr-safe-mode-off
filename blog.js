3dc3b5896e083501a9dc758aefe4358a7adcba80
Bitti <Bitti09@users.noreply.github.com>
2021-08-13 23:57:11 +0200

    Add files via upload

--- blog.js
+++ blog.js
@@ -1,10 +1,14 @@
 // automatically click "View this Tumblr"
 const observer = new MutationObserver((_, self) => {
+	console.log("test1");
     const showMeButton = document.querySelector('button[aria-label="View this Tumblr" i]'); // fragile - class names are all generated
     if (showMeButton) {
-        document.querySelector('._3uGiA').classList.add('blog-fullscreen'); // only show blog fullscreen if safe-view
+	console.log("test2");
+        document.querySelector('._3otBh').classList.add('blog-fullscreen'); // only show blog fullscreen if safe-view
+        document.querySelector('._15MuE').style ="max-width: none !important";// only show blog fullscreen if safe-view
+        document.querySelector('._1cR6l').style.width = "100%"; // only show blog fullscreen if safe-view
         showMeButton.click();
-        // self.disconnect();
+        self.disconnect();
     }
 });
 observer.observe(document.body, {
